93192865cd480cc83ad84895400f45ee308e1878
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 21:29:54 +0530

    Rjoin column with studentrid and orderlines

--- SecondChance/src/main/java/com/example/demo/model/FoodOrder.java
+++ SecondChance/src/main/java/com/example/demo/model/FoodOrder.java
@@ -1,13 +1,25 @@
 package com.example.demo.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class FoodOrder {
 	@Id
 	private Long id;
 	private LocalDate orderDate;
+	
+	@ManyToOne
+	@JoinColumn(name="student_id")
+	private Student student;
+	
+	@OneToMany(mappedBy = "foodorder")
+	private List<OrderLine> orderLines;
 }
