c7aefe928e6e76d394cf61a6c3c84e14580350fa
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 21:46:51 +0530

    create relationship studnet with food orders

--- SecondChance/src/main/java/com/example/demo/model/Student.java
+++ SecondChance/src/main/java/com/example/demo/model/Student.java
@@ -1,6 +1,15 @@
 package com.example.demo.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Student extends Person {
 	private String acadamicDegree;
+	
+	@OneToMany(mappedBy = "student")
+	private List<FoodOrder> foodOrders;
 
 }
