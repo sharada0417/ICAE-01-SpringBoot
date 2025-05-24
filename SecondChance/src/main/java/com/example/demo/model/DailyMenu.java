ed023c2a8a72208bf4edcddf67da364b901e5b6d
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 21:13:31 +0530

    join tables with columns food item name;

--- SecondChance/src/main/java/com/example/demo/model/DailyMenu.java
+++ SecondChance/src/main/java/com/example/demo/model/DailyMenu.java
@@ -1,9 +1,13 @@
 package com.example.demo.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class DailyMenu {
@@ -11,4 +15,15 @@ public class DailyMenu {
 	private Long id;
 	private LocalDate date;
 	
+	@ManyToMany
+	@JoinColumn(name="canteen_id")
+	private Canteen canteen;
+	
+	@ManyToMany
+	@JoinTable(
+		name = "menu_food_item",
+		joinColumns = @JoinColumn(name = "menu_id"),
+		inverseJoinColumns = @JoinColumn(name="food_item_id")
+	)
+	private List<FoodItem> foodItems;
 }
