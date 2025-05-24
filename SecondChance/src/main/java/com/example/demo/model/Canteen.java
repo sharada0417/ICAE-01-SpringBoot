20d2ba59ae275150d0fca126d7e2dac3b3483ca4
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 21:08:51 +0530

    create relationship between Employees and canteen , dailymenus;

--- SecondChance/src/main/java/com/example/demo/model/Canteen.java
+++ SecondChance/src/main/java/com/example/demo/model/Canteen.java
@@ -1,7 +1,11 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Canteen {
@@ -10,5 +14,11 @@ public class Canteen {
 	
 	private String name;
 	private String Location;
+	
+	@ManyToMany(mappedBy = "canteens")
+	private List<Employee> employees;
+	
+	@OneToMany
+	private List<DailyMenu> dailyMenus;
 	
 }
